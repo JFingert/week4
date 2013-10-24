6c8be4608155508a417b25889cd6f90e437af10f
JFingert <joshuafingert@gmail.com>
2013-10-24 16:51:10 -0700

    first commit

---
+++ fortuneTeller.js
@@ -0,0 +1,13 @@
+fortunes = ["Morning doobies will be daily", 
+"Might fly like a Cardinal until...", 
+"A dubious friend may be an enemy in camouflage.",
+ "A golden egg of opportunity falls into your lap this month.",
+  "A smile is your personal welcome mat.", 
+  "All your hard work will soon pay off.",
+   "Beauty in its various forms appeals to you. (2)",
+    "Better ask twice than lose yourself once.",
+     "An inch of time is an inch of gold.",
+      "Believe it can be done."];
+
+
+console.log(fortunes[Math.floor((Math.random() * 9) + 1)]);
