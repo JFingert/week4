83e7b72aef8056064d4eda34e7833bf2b16697b6
JFingert <joshuafingert@gmail.com>
2013-10-25 10:39:55 -0700

    initial commit

--- constructorShoppingCart.js
+++ constructorShoppingCart.js
@@ -11,27 +11,19 @@ ShoppingCart.prototype.cost = function () {
 }
 
 ShoppingCart.prototype.getId = function (id) {
-	//this.descId.forEach(function(item, index, arr) {
-      var lilName = " ";
-      if (this.id) {
-      	return this.id;
-      } else {
-      	lilName = this.desc.substr(0,2) + Math.floor(Math.random() * 1000);
-      	this.id = lilName;
-      }
+  var lilName = " ";
+  if (this.id) {
+   return this.id;
+  } else {
+    lilName = this.desc.substr(0,2) + Math.floor(Math.random() * 1000);
+    this.id = lilName;
+  }
       
-    return lilName;
+  return lilName;
 }
 
 var Marbles = new ShoppingCart ("Blue", 31, 2.99)
 console.log(Marbles.getId());
 console.log(Marbles.cost());
 	
-  //console.log(this.lilName);
-//console.log(ShoppingCart.lilName);
-
-/* ShoppingCart.cost({desc: "Huggies Little Snugglers Diapers",
-                              count: 4,
-                              price: 19.77});
-
-console.log("Your total for " + lilName + " is: " + ShoppingCart.cost());  */
+  
